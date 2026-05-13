8b8203ef3b0294a78d63d44fd28990ffbce9ce84
Michael Redig <mredig@gmail.com>
2026-05-13 02:11:55 -0500

    refactor: a bit more org

--- Sources/TestSupport/MockingServer.swift
+++ Sources/TestSupport/MockingServer.swift
@@ -40,7 +40,10 @@ public class MockingServer {
 		}
 	}
 
-	public typealias Endpoint = @Sendable (_ request: IncomingRequest, _ stream: ResponseStreamer.Block) async throws -> Void
+	public typealias Endpoint = @Sendable (
+		_ request: IncomingRequest,
+		_ stream: ResponseStream.Block
+	) async throws -> Void
 
 	private struct EndpointPath: Hashable {
 		let path: Path
@@ -70,7 +73,7 @@ public class MockingServer {
 			let startResponseWrapper = Sendify(startResponse)
 			let sendBodyWrapper = Sendify(sendBody)
 
-			let tracker = ResponseStreamer { chunk in
+			let tracker = ResponseStream.LifecycleTracker { chunk in
 				switch chunk {
 				case .header(let header):
 					let responseCodePhrase = Self.reasonPhrases[header.responseCode] ?? "OK"
@@ -104,7 +107,7 @@ public class MockingServer {
 	private func runServerLogic(
 		env: [String: Any],
 		runLoop: EventLoop,
-		responseStream: ResponseStreamer
+		responseStream: ResponseStream.LifecycleTracker
 	) {
 		guard
 			let pathStr = env["PATH_INFO"] as? String,
@@ -247,75 +250,80 @@ public class MockingServer {
 		504: "Gateway Timeout",
 	]
 
-	public final class ResponseStreamer: @unchecked Sendable {
-		private let lock = MutexLock()
-
-		private struct State: Hashable, Sendable {
-			var hasResponded = false
-			var hasSentData = false
-			var hasCompleted = false
-		}
-
-		nonisolated(unsafe)
-		private var state = State()
-
-		private let streamBlock: Block
-
-		init(_ streamBlock: @escaping Block) {
-			self.streamBlock = streamBlock
-		}
-
-		deinit {
-			lock.withLock {
-				if state.hasResponded == false {
-					_stream(.header(.init(responseCode: 500)))
-				}
-				if state.hasCompleted == false {
-					_stream(.complete)
-				}
-			}
-		}
-
-		private func _stream(_ chunk: Chunk) {
-			guard state.hasCompleted == false else { return }
-
-			switch chunk {
-			case .header:
-				guard state.hasResponded == false else { return }
-				state.hasResponded = true
-			case .data:
-				if state.hasResponded == false {
-					streamBlock(
-						.header(
-							.init(responseCode: 500, responseHeader: [#HTTPFieldName("Error"): "Did not send response before data"])))
-				}
-				state.hasSentData = true
-			case .complete:
-				if state.hasResponded == false {
-					streamBlock(
-						.header(
-							.init(responseCode: 500, responseHeader: [#HTTPFieldName("Error"): "Did not send response before completion"])))
-				}
-				state.hasCompleted = true
-			}
-
-			streamBlock(chunk)
-		}
-
-		func stream(_ chunk: Chunk) {
-			lock.withLock { _stream(chunk) }
-		}
-
-		func callAsFunction(_ chunk: Chunk) {
-			stream(chunk)
-		}
-
+	public enum ResponseStream {
 		public enum Chunk: Sendable {
 			case header(OutboundResponseHeader)
 			case data(Data)
 			case complete
 		}
 		public typealias Block = @Sendable (Chunk) -> Void
+
+		/// Created on the server on each request. The server passes the base server's chunk block to it, then
+		/// executes the request from the endpoint. The tracker, as implied, tracks and enforces that the response
+		/// is sent first, followed by data, if any, followed by completion.
+		final class LifecycleTracker: @unchecked Sendable {
+			private let lock = MutexLock()
+
+			private struct State: Hashable, Sendable {
+				var hasResponded = false
+				var hasSentData = false
+				var hasCompleted = false
+			}
+
+			nonisolated(unsafe)
+			private var state = State()
+
+			private let streamBlock: Block
+
+			init(_ streamBlock: @escaping Block) {
+				self.streamBlock = streamBlock
+			}
+
+			deinit {
+				lock.withLock {
+					if state.hasResponded == false {
+						_stream(.header(.init(responseCode: 500)))
+					}
+					if state.hasCompleted == false {
+						_stream(.complete)
+					}
+				}
+			}
+
+			private func _stream(_ chunk: Chunk) {
+				guard state.hasCompleted == false else { return }
+
+				switch chunk {
+				case .header:
+					guard state.hasResponded == false else { return }
+					state.hasResponded = true
+				case .data:
+					if state.hasResponded == false {
+						streamBlock(
+							.header(
+								.init(responseCode: 500, responseHeader: [#HTTPFieldName("Error"): "Did not send response before data"])))
+					}
+					state.hasSentData = true
+				case .complete:
+					if state.hasResponded == false {
+						streamBlock(
+							.header(
+								.init(responseCode: 500, responseHeader: [#HTTPFieldName("Error"): "Did not send response before completion"])))
+					}
+					state.hasCompleted = true
+				}
+
+				streamBlock(chunk)
+			}
+
+			func stream(_ chunk: Chunk) {
+				lock.withLock { _stream(chunk) }
+			}
+
+			func callAsFunction(_ chunk: Chunk) {
+				stream(chunk)
+			}
+		}
 	}
 }
 
