62719000b4bbc773d94eb2204e43c54792fcedaf
Michael Redig <mredig@gmail.com>
2026-05-26 00:09:05 -0500

    refactor: update AHC tests

--- Tests/NetworkHandlerAHCTests/NetworkHandlerAHCTests.swift
+++ Tests/NetworkHandlerAHCTests/NetworkHandlerAHCTests.swift
@@ -10,121 +10,117 @@ import TestSupport
 struct NetworkHandlerAHCTests: Sendable {
 	let commonTests = NetworkHandlerCommonTests<HTTPClient>(logger: Logger(label: #fileID))
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func downloadAndCacheImages() async throws {
 		let mockingEngine = generateEngine()
 
-		let lighthouseURL = try #require(Bundle.testBundle.url(forResource: "lighthouse", withExtension: "jpg"))
-		let lighthouseData = try Data(contentsOf: lighthouseURL)
-
-		try await commonTests.downloadAndCacheImages(engine: mockingEngine, imageExpectationData: lighthouseData)
+		try await commonTests.downloadAndCacheImages(
+			engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func downloadAndDecodeData() async throws {
 		let mockingEngine = generateEngine()
 
-		let modelURL = commonTests.demoModelURL
-
-		let testModel = try DemoModel(
-			id: #require(UUID(uuidString: "59747267-D47D-47CD-9E54-F79FA3C1F99B")),
-			title: "FooTitle",
-			subtitle: "BarSub",
-			imageURL: commonTests.imageURL)
-
-		try await commonTests.downloadAndDecodeData(engine: mockingEngine, modelURL: modelURL, expectedModel: testModel)
+		try await commonTests.downloadAndDecodeData(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func handle404() async throws {
 		let mockingEngine = generateEngine()
 
-		let demo404URL = commonTests.demo404URL
-
 		try await commonTests.handle404Error(
-			engine: mockingEngine,
-			expectedError: NetworkError.httpUnexpectedStatusCode(
-				code: 404,
-				originalRequest: .standard(demo404URL.generalRequest),
-				data: nil))
+			engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func expect200OnlyGet200() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.expect200OnlyGet200(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func expect201OnlyGet200() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.expect201OnlyGet200(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadData() async throws {
 		let mockingEngine = generateEngine()
 		try await commonTests.uploadData(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadFileURL() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.uploadFileURL(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadMultipartFile() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.uploadMultipartFile(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadMultipartStream() async throws {
 		let mockingEngine = generateEngine()
+
 		try await commonTests.uploadMultipartStream(engine: mockingEngine)
 	}
 
-	@Test func badCodingData() async throws {
-		let mockingEngine = generateEngine()
-
-		try await commonTests.badCodableData(engine: mockingEngine)
-	}
-
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func cancellationViaToken() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.cancellationViaToken(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func cancellationViaStream() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.cancellationViaStream(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadCancellationViaToken() async throws {
 		let mockingEngine = generateEngine()
 		try await commonTests.uploadCancellationViaToken(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func timeoutTriggersRetry() async throws {
 		let mockingEngine = generateEngine()
 		try await commonTests.timeoutTriggersRetry(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func downloadProgressTracking() async throws {
 		let mockingEngine = generateEngine()
 		try await commonTests.downloadProgressTracking(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func uploadProgressTracking() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.uploadProgressTracking(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func polling() async throws {
 		let mockingEngine = generateEngine()
 
 		try await commonTests.polling(engine: mockingEngine)
 	}
 
+	@available(macOS 15.0.0, iOS 18.0.0, tvOS 18.0.0, *)
 	@Test func downloadFile() async throws {
 		let mockingEngine = generateEngine()
 
